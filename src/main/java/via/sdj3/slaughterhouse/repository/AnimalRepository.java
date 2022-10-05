fcb624b7fa0591a680ad79f0ff1d33777e8a764a
Simon Mayer <315183@via.dk>
2022-10-05 10:30:28 +0200

    interfaces created

--- src/main/java/via/sdj3/slaughterhouse/repository/AnimalRepository.java
+++ src/main/java/via/sdj3/slaughterhouse/repository/AnimalRepository.java
@@ -1,5 +1,8 @@
 package via.sdj3.slaughterhouse.repository;
 
+import via.sdj3.slaughterhouse.model.Animal;
+
 public interface AnimalRepository
 {
+    Animal registerAnimal(Animal animal);
 }
--- src/main/java/via/sdj3/slaughterhouse/service/AnimalService.java
+++ src/main/java/via/sdj3/slaughterhouse/service/AnimalService.java
@@ -1,5 +1,8 @@
 package via.sdj3.slaughterhouse.service;
 
+import via.sdj3.slaughterhouse.model.Animal;
+
 public interface AnimalService
 {
+    Animal registerAnimal(Animal animal);
 }
