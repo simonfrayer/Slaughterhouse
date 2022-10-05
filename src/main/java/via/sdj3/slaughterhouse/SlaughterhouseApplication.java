527b9d45025229da044d4297c99e34f9aa2757f1
Simon Mayer <315183@via.dk>
2022-10-05 10:13:33 +0200

    Initial commit

---
+++ src/main/java/via/sdj3/slaughterhouse/SlaughterhouseApplication.java
@@ -0,0 +1,13 @@
+package via.sdj3.slaughterhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlaughterhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlaughterhouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/via/sdj3/slaughterhouse/SlaughterhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package via.sdj3.slaughterhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlaughterhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
