a7a4c323f07e96060801a8a2a1f12d9be2994953
Simon Mayer <315183@via.dk>
2022-10-05 12:18:19 +0200

    getAll finished

--- src/main/java/via/sdj3/slaughterhouse/repository/AnimalRepositoryImpl.java
+++ src/main/java/via/sdj3/slaughterhouse/repository/AnimalRepositoryImpl.java
@@ -3,6 +3,7 @@ package via.sdj3.slaughterhouse.repository;
 import org.springframework.stereotype.Repository;
 import via.sdj3.slaughterhouse.model.Animal;
 
+import java.sql.Timestamp;
 import java.util.*;
 
 @Repository
@@ -10,6 +11,12 @@ public class AnimalRepositoryImpl implements AnimalRepository
 {
 
   private static final Map<Long, Animal> animalMap = new HashMap<>();
+
+  public AnimalRepositoryImpl() {
+    Animal animal = new Animal(new Timestamp(new Date().getTime()), 60, 1, "Farm1");
+
+    animalMap.put(1L,animal);
+  }
 
   @Override public Animal registerAnimal(Animal animal)
   {
