116fab845dcf57c3664c693f9c1c6589d2728094
agata <315233@via.dk>
2022-10-05 10:45:03 +0200

    Animal repository implementation(register animal)

--- src/main/java/via/sdj3/slaughterhouse/repository/AnimalRepositoryImpl.java
+++ src/main/java/via/sdj3/slaughterhouse/repository/AnimalRepositoryImpl.java
@@ -1,5 +1,21 @@
 package via.sdj3.slaughterhouse.repository;
 
-public class AnimalRepositoryImpl
+import org.springframework.stereotype.Repository;
+import via.sdj3.slaughterhouse.model.Animal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class AnimalRepositoryImpl implements AnimalRepository
 {
+
+  private static final Map<Long, Animal> animalMap = new HashMap<>();
+
+  @Override public Animal registerAnimal(Animal animal)
+  {
+    animalMap.put(animal.getRegistrationNumber(),animal);
+    return animal;
+  }
+
 }
