34ebc279d9193aa9abfd3c0d9ff070161c95bab9
Simon Mayer <315183@via.dk>
2022-10-05 11:18:41 +0200

    some changes setup

--- src/main/java/via/sdj3/slaughterhouse/service/AnimalService.java
+++ src/main/java/via/sdj3/slaughterhouse/service/AnimalService.java
@@ -1,6 +1,5 @@
 package via.sdj3.slaughterhouse.service;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import via.sdj3.slaughterhouse.model.Animal;
 
