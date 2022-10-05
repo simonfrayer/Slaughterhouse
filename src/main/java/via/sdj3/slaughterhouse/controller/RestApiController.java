d8b93cdd7a362a1057e762f35518d3b73171de42
Simon Mayer <315183@via.dk>
2022-10-05 11:09:46 +0200

    nothing

--- src/main/java/via/sdj3/slaughterhouse/controller/RestApiController.java
+++ src/main/java/via/sdj3/slaughterhouse/controller/RestApiController.java
@@ -1,6 +1,5 @@
 package via.sdj3.slaughterhouse.controller;
 
-import org.hibernate.criterion.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
